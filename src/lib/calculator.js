3f776967aef6e38abb2f024695c7e56461665413
Bruno Carvalho Cagni <brunocarvalhocagni@outlook.com>
2022-03-11 11:22:49 -0300

    commit final

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,9 +1,9 @@
-module.exports.sum = (num1, num2) =>{
+export function sum(num1, num2){
 
     const int1 = parseInt(num1, 10);
     const int2 = +num2;
     if(Number.isNaN(int1) || Number.isNaN(int2)){
         throw new Error("please check your input");
     }
-    return +int1 + +int2;
+    return int1 + int2;
 }
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,5 +1,5 @@
 
-const {queryString} = require('./queryString')
+const {queryString, parse} = require('./queryString')
 describe('Object to query string', () =>{
 it('should create a valid query string when an object is provided', () =>{
     const obj ={
@@ -31,5 +31,34 @@ it('should create a valid query string when an array is provided', () =>{
         queryString(obj);
     }).toThrowError();
 });
+
+});
+
+describe('Query string to object', () => {
+    it('should convert a query string to object', () =>{
+        const qs = 'name=Bruno&profession=developer';
+        
+        expect(parse(qs)).toEqual({
+         name:'Bruno',
+            profession:'developer'
+        })
+    });
+    it('should convert a query string to object from a single key-value', () =>{
+        const qs = 'name=Bruno';
+        
+        expect(parse(qs)).toEqual({
+         name:'Bruno',
+        })
+    });
+
+    it('should convert a query string to object taking care of coma separeted values', () =>{
+        const qs = 'name=Bruno&abilities=JS,node';
+        
+        expect(parse(qs)).toEqual({
+         name:'Bruno',
+         abilities: ['JS', 'node']
+        })
+    });
+
 
 });
